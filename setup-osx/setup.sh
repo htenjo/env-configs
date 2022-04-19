8eb273a734ff56fb64886e91df878353fde45681
Hernan Tenjo <htenjomateus@twilio.com>
2022-04-19 00:35:24 -0500

    Fixing new line for confirmation messages

--- setup-osx/setup.sh
+++ setup-osx/setup.sh
@@ -12,6 +12,7 @@ requestPrivileges() {
 
 setupOsxUpdates() {
   if read -q "?Would you like to update you OSX? (y/n): " ; then
+    echo
     echo "::: Updating OSX ..."
   	sudo softwareupdate -ia --verbose
   else
@@ -21,6 +22,7 @@ setupOsxUpdates() {
 
 setupXCode() {
   if  read -q "?Would you like to install Xcode DevTools? (y/n): "  ; then
+    echo
     echo "::: Installing Xcode Command Line Tools"
     xcode-select --install
   else
@@ -30,6 +32,7 @@ setupXCode() {
 
 setupCocoaPods() {
   if  read -q "?Would you like to install CocoaPods DevTools? (y/n): "  ; then
+    echo
   	echo "::: Installing CocoaPods"
   	sudo gem install cocoapods
   else
@@ -39,6 +42,7 @@ setupCocoaPods() {
 
 setupHomebrew() {
   if  read -q "?Would you like to install homebrew dev tools? (y/n): "  ; then
+    echo
   	echo "::: Installing homebrew"
   	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   else
@@ -48,7 +52,8 @@ setupHomebrew() {
 
 setupJEnv() {
   if  read -q "?Would you like to install JEnv? (y/n): "  ; then
-  	echo ::: Installing JEnv
+    echo
+  	echo "::: Installing JEnv"
   	brew install jenv
   	echo 'eval "$(jenv init -)"' >> $ENV_FILE
   	source $ENV_FILE
@@ -64,6 +69,7 @@ setupJEnv() {
 
 setupOracleJDK() {
   if  read -q "?Would you like to install the Oracle JDK? (y/n): "  ; then
+    echo
   	brew install --cask oracle-jdk
   	echo "::: Oracle JDK OK"
   	jenv add $(/usr/libexec/java_home)
@@ -75,6 +81,7 @@ setupOracleJDK() {
 
 setupOpenJDK() {
   if  read -q "?Would you like to install OpenJDK 8? (y/n): "  ; then
+    echo
   	brew tap adoptopenjdk/openjdk
   	brew install --cask adoptopenjdk8
   	echo "::: OpenJDK 8 OK"
@@ -85,6 +92,7 @@ setupOpenJDK() {
 
 setupMaven() {
   if  read -q "?Would you like to install Maven? (y/n): "  ; then
+    echo
   	echo "::: Installing Maven..."
   	brew install maven
   	echo "::: Maven OK"
@@ -95,6 +103,7 @@ setupMaven() {
 
 setupGradle() {
   if  read -q "?Would you like to install Gradle? (y/n): "  ; then
+    echo
   	echo "::: Installing Gradle"
   	brew install gradle
   	echo "::: Gradle OK"
@@ -105,6 +114,7 @@ setupGradle() {
 
 setupIntelliJCE() {
   if  read -q "?Would you like to install IntelliJ CE? (y/n): "  ; then
+    echo
     echo "::: Installing IntelliJ CE..."
     brew install --cask intellij-idea
     echo "::: IntelliJ OK"
@@ -115,6 +125,7 @@ setupIntelliJCE() {
 
 setupGo() {
   if read -q "?Would you like to install Go (y/n) " ; then
+    echo
   	echo "::: Installing Go ..."
     brew install golang
   	echo '::: Configuring Go ...'
@@ -131,6 +142,7 @@ setupGo() {
 
 setupFlutter() {
   if  read -q "?Would you like to install Flutter? (y/n): "  ; then
+    echo
     echo "::: Downloading Flutter ..."
   	wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.20.1-stable.zip
   	mv flutter_macos_1.20.1-stable.zip flutter.zip
@@ -148,6 +160,7 @@ setupFlutter() {
 
 setupNodeJS() {
   if  read -q "?Would you like to install NodeJS? (y/n): "  ; then
+    echo
   	echo "::: Installing NodeJS..."
   	brew install node
   	echo "::: NodeJS OK"
@@ -158,6 +171,7 @@ setupNodeJS() {
 
 setupYarn() {
   if  read -q "?Would you like to install Yarn? (y/n): "  ; then
+    echo
     echo "::: Installing Yarn ..."
   	brew install yarn
   	echo "::: Yarn OK"
@@ -168,6 +182,7 @@ setupYarn() {
 
 setupGulp() {
   if  read -q "?Would you like to install Gulp? (y/n): "  ; then
+    echo
     echo "::: Installing Gulp ..."
   	npm install -g gulp-cli
   	echo "::: Gulp OK"
@@ -178,6 +193,7 @@ setupGulp() {
 
 setupWGet() {
   if  read -q "?Would you like to install WGet? (y/n): "  ; then
+    echo
     echo "::: Installing WGet ..."
   	brew install wget
   	echo "::: Wget OK"
@@ -188,6 +204,7 @@ setupWGet() {
 
 setupITerm() {
   if  read -q "?Would you like to install iTerm2? (y/n): "  ; then
+    echo
     echo "::: Installing iTerm2 ..."
   	brew install --cask iterm2
   	echo "::: Downloading themes from https://iterm2colorschemes.com"
@@ -201,6 +218,7 @@ setupITerm() {
 
 setupVSCode() {
   if  read -q "?Would you like to install VSCode? (y/n): "  ; then
+    echo
     echo "::: Installing VSCode ..."
   	brew install --cask visual-studio-code
   else
@@ -210,6 +228,7 @@ setupVSCode() {
 
 setupVSCodeExtensions() {
   if  read -q "?Would you like to install Material, Markdown, and basic-generic VSCode Extensions? (y/n): "  ; then
+    echo
     echo "::: Installing VSCode Extensions ..."
   	code --install-extension
   	code --install-extension DotJoshJohnson.xml
@@ -223,6 +242,7 @@ setupVSCodeExtensions() {
 
 setupVsCodeFlutterExtension() {
   if  read -q "?Would you like to install Flutter VSCode Extensions? (y/n): "  ; then
+    echo
     echo "::: Installing VSCode Extensions ..."
   	code --install-extension
   	code --install-extension Dart-Code.flutter
@@ -233,6 +253,7 @@ setupVsCodeFlutterExtension() {
 
 setupVsCodeJavaExtension() {
   if  read -q "?Would you like to install Java VSCode Extensions? (y/n): "  ; then
+    echo
     echo "::: Installing VSCode Extensions ..."
   	code --install-extension
   	code --install-extension redhat.java
@@ -248,6 +269,7 @@ setupVsCodeJavaExtension() {
 
 setupDocker() {
   if  read -q "?Would you like to install Docker? (y/n): "  ; then
+    echo
   	echo "::: Installing Docker ..."
   	brew install --cask docker
   else
@@ -257,8 +279,9 @@ setupDocker() {
 
 setupTerraform() {
   if  read -q "?Would you like to install Terraform? (y/n): "  ; then
-      echo "::: Installing Terraform ..."
-      brew tap hashicorp/tap
+    echo
+    echo "::: Installing Terraform ..."
+    brew tap hashicorp/tap
   	brew install hashicorp/tap/terraform
   	brew upgrade hashicorp/tap/terraform
   	echo ::: Terraform OK
@@ -269,6 +292,7 @@ setupTerraform() {
 
 setupSlack() {
   if read -q "?Would you like to install Slack? (y/n): "  ; then
+    echo
     echo "::: Installing Slack ..."
   	brew install --cask slack
   else
@@ -278,6 +302,7 @@ setupSlack() {
 
 setupChrome() {
   if read -q "?Would you like to install Chrome? (y/n): "  ; then
+    echo
     echo "::: Installing Google Chrome ..."
   	brew install --cask google-chrome
   else
@@ -287,6 +312,7 @@ setupChrome() {
 
 setupDBeaver() {
   if read -q "?Would you like to install DBeaver? (y/n): "  ; then
+    echo
   	echo "::: Installing DBeaver ..."
   	brew install --cask dbeaver-community
   else
@@ -296,6 +322,7 @@ setupDBeaver() {
 
 setupSpotify() {
   if  read -q "?Would you like to install Spotify? (y/n): "  ; then
+    echo
   	echo "::: Installing Spotify ..."
   	brew install --cask spotify
   else
@@ -305,6 +332,7 @@ setupSpotify() {
 
 setupUnArchiver() {
   if  read -q "?Would you like to install UnArchiver? (y/n): "  ; then
+    echo
   	echo "::: Installing UnArchiver ..."
   	brew install --cask the-unarchiver
   else
@@ -314,6 +342,7 @@ setupUnArchiver() {
 
 setupPostman() {
   if  read -q "?Would you like to install Postman? (y/n): "  ; then
+    echo
   	echo "::: Installing Postman ..."
   	brew install --cask postman
   else
@@ -323,6 +352,7 @@ setupPostman() {
 
 setupVLC() {
   if  read -q "?Would you like to install VLC? (y/n): "  ; then
+    echo
   	echo "::: Installing VLC ..."
   	brew install --cask vlc
   else
@@ -332,6 +362,7 @@ setupVLC() {
 
 setupPgAdmin() {
   if  read -q "?Would you like to install PgAdmin4? (y/n): "  ; then
+    echo
   	echo "::: Installing PgAdmin4 ..."
   	brew install --cask pgAdmin4
   else
@@ -341,6 +372,7 @@ setupPgAdmin() {
 
 setupWhatsapp() {
   if  read -q "?Would you like to install Whatsapp? (y/n): "  ; then
+    echo
   	echo "::: Installing Whatsapp ..."
   	brew install --cask whatsapp
   else
@@ -350,6 +382,7 @@ setupWhatsapp() {
 
 setupWebex() {
   if  read -q "?Would you like to install Webex? (y/n): "  ; then
+    echo
   	echo "::: Installing Webex ..."
   	brew install --cask webex-meetings
   else
@@ -359,6 +392,7 @@ setupWebex() {
 
 setupOhMyZsh() {
   if  read -q "?Would you like to install oh-my-zsh (y/n): "  ; then
+    echo
   	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   	compaudit | xargs chmod g-w,o-w
   else
@@ -368,6 +402,7 @@ setupOhMyZsh() {
 
 setupEnvironmentVariables() {
   if  read -q "?Would you like to configure your ENV (.zshenv, .zshrc) (y/n): "  ; then
+    echo
   	chmod +x env_config.sh
   	source ./env_config.sh
   else
@@ -377,6 +412,7 @@ setupEnvironmentVariables() {
 
 setupSshKeys() {
   if  read -q "?Would you like to create SSH Keys (y/n): "  ; then
+    echo
   	echo "::: Type same filenames to be able to add the keys to the keymananger"
   	echo "::: Generating SSH-KEY 1: id_rcc ]"
   	ssh-keygen -t rsa -C "RCC"
@@ -390,6 +426,7 @@ setupSshKeys() {
 
 setupTLDR() {
   if  read -q "?Would you like to install TLDR (Simple MAN info) (y/n): "  ; then
+    echo
   	echo "::: Installing TLDR ... "
   	npm install -g tldr
   else
@@ -399,6 +436,7 @@ setupTLDR() {
 
 setupGitConfig() {
   if  read -q "?Would you like to configure your git name and email (y/n): "  ; then
+    echo
     echo '::: Configuring git ...'
     git config --global user.email "hernan.tenjo@gmail.com"
     git config --global user.name "Hernan Tenjo"
